6a547b60080a4d809acf49cfbd5df3582a74fd93
rhommardev <rjmlp02@gmail.com>
2025-08-07 01:07:25 -0300

    configuracao migrations veiculos

---
+++ Api/Migrations/20250807040157_veiculosMigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mininal_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class veiculosMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
